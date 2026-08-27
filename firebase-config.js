e3b7c58989a81da6535dfa3d672da188378cd86b
utec-math <martin.bulanti@utec.edu.uy>
2026-08-27 19:01:27 -0300

    Configure Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2vgqvLLwcJYPc0gca2pC_ud0q31sxkXY",
+  authDomain: "preciencia1.firebaseapp.com",
+  databaseURL: "https://preciencia1-default-rtdb.firebaseio.com/",
+  projectId: "preciencia1",
+  storageBucket: "preciencia1.appspot.com",
+  messagingSenderId: "1032667684315",
+  appId: "1:1032667684315:web:cb64622170b3280d691bfe",
+  measurementId: "G-D42XDQW2LE"
+};
